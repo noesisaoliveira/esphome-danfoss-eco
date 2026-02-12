31c3d762ece86be5083fd87ce3e3350b1033d5c7
noesisaoliveira <alberto.j.oliveira@parceiros.nos.pt>
2026-02-12 19:42:30 +0000

    update

--- components/danfoss_eco/climate.py
+++ components/danfoss_eco/climate.py
@@ -43,9 +43,9 @@ def validate_pin(value):
         raise cv.Invalid("PIN code should be numeric")
     return value
 
-# Build schema manually since CLIMATE_SCHEMA doesn't exist in this version
+# Patched: Using CLIMATE_SCHEMA.extend to include the mandatory 'visual' block
 CONFIG_SCHEMA = cv.All(
-    cv.Schema(
+    climate.CLIMATE_SCHEMA.extend(
         {
             cv.GenerateID(): cv.declare_id(DanfossEco),
             cv.Optional(CONF_SECRET_KEY): validate_secret,
@@ -70,11 +70,9 @@ CONFIG_SCHEMA = cv.All(
         }
     )
     .extend(cv.COMPONENT_SCHEMA)
-    .extend(cv.ENTITY_BASE_SCHEMA)
     .extend(ble_client.BLE_CLIENT_SCHEMA)
     .extend(cv.polling_component_schema("60s"))
 )
-
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
